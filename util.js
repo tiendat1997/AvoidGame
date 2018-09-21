19c2252a3a5c2bb8c95d1968428c49dbcadcfef7
tiendat <tiendatdeveloper1997@gmail.com>
2018-09-21 21:14:25 +0700

    Adding source code game

---
+++ util.js
@@ -0,0 +1,51 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomColorRGB() {
+  return `rgb(${getRandomInt(0, 255)},${getRandomInt(0, 255)},${getRandomInt(0, 255)} )`;
+}
+
+function showPlayAlert() {
+  swal({
+    title: "AVOID AVOID",
+    text: "--- 1 MINUTE, 30 SECONDS to WIN ---",
+    icon: "game-icon.png",
+    iconWidth: '100',
+    iconHeight: '100',
+    button: {
+      text: "Play",
+      closeModal: true,
+    },
+    closeOnClickOutside: false
+  }).then((value) => {
+    startGame();
+  });
+}
+
+function showLoseAlert() {
+  swal({
+    title: `Your score: ${score}`,
+    icon: "lose-icon.jpg",
+    button: {
+      text: "Replay",
+      closeModal: true
+    },
+    dangerMode: true,
+    closeOnClickOutside: false
+  })
+    .then((value) => {
+      showPlayAlert();
+    });
+}
+
+function showWinAlert() {
+  swal({
+    title: "^^ YOU ARE WIN ^^",    
+    icon: "success",    
+  }).then((value) => {
+    showPlayAlert();
+  });
+}
